8fc3476758c9cc9495e4f14cd751b8516039d05d
yel106 <yel_@hanmail.net>
2023-10-20 16:38:53 +0900

    inputStream outputStream 파일 보내기, AWS

--- src/main/java/com/example/spring20230920/controller/Controller30.java
+++ src/main/java/com/example/spring20230920/controller/Controller30.java
@@ -26,7 +26,7 @@ public class Controller30 {
 
     @GetMapping("sub1")
     public void method1(Integer id) {
-        String name= dao.select1(id); //n번 id 고객의 이름을 알수있도록
+        String name = dao.select1(id); //n번 id 고객의 이름을 알수있도록
         System.out.println("name = " + name);
 
     }
@@ -34,106 +34,114 @@ public class Controller30 {
 
     @GetMapping("sub2")
     public void method2(Double from, Double to) {
-        List<String > names = dao.select2(from, to); // 두개의 파라미터 받게 함
+        List<String> names = dao.select2(from, to); // 두개의 파라미터 받게 함
         names.forEach(System.out::println);
     }
 
     // /main30/sub3?from=1996-08-01&to=1996-08-31 이런식으로 요청
-   @GetMapping("sub3")
-   // from~to 날짜 사이에 주문된 상품명 출력( 정렬해서, 중복없이 조회)
+    @GetMapping("sub3")
+    // from~to 날짜 사이에 주문된 상품명 출력( 정렬해서, 중복없이 조회)
     public void method3(LocalDate from, LocalDate to) {
         List<String> list = dao.select3(from, to);
         list.forEach(System.out::println);
-       System.out.println("list.size() = " + list.size());
-   }
+        System.out.println("list.size() = " + list.size());
+    }
 
-//    /main30/sub4?min=1.00&max=80.00
-   @GetMapping("sub4")
+    //    /main30/sub4?min=1.00&max=80.00
+    @GetMapping("sub4")
     public void method4(MyDto25 dto) {   //파라미터로 MyDto 쓰면 아래 MyDto25의 객체 생성 안해도 됨
 //        MyDto25 dto = new MyDto25();
 //        dto.setMin(1.00);
 //        dto.setMax(80.00);
-       List<String> list = dao.select4(dto);//MyDto25를 만들고 프로퍼티를 꺼내서 쓰는 방법
-       System.out.println("list.size() = " + list.size());
-   }
+        List<String> list = dao.select4(dto);//MyDto25를 만들고 프로퍼티를 꺼내서 쓰는 방법
+        System.out.println("list.size() = " + list.size());
+    }
 
 
-
-   // /main30/sub5?country1=germany&country2=france
-   @GetMapping("sub5")
+    // /main30/sub5?country1=germany&country2=france
+    @GetMapping("sub5")
     public void method5(MyDto26 dto) {
         // country1 또는 country2에 사는 고객명 출력
         List<String> list = dao.select5(dto);
-       list.forEach(System.out::println);
-   }
+        list.forEach(System.out::println);
+    }
 
 
-//     /main30/sub6?from=1996-09-01&to=1996-09-30
-   @GetMapping("sub6")
+    //     /main30/sub6?from=1996-09-01&to=1996-09-30
+    @GetMapping("sub6")
     public void method6(MyDto27 dto1, MyDto28 dto2) {  //파라미터가 두 개 넘어가는 경우
         // 리퀘스트 파람 from에는 dto1이 세팅되고, to에는 dto2가 세팅됨
         Integer num = dao.select6(dto1, dto2);
-       System.out.println("num = " + num);
-   }
+        System.out.println("num = " + num);
+    }
 
 
-   // /main30/sub7?page=2&rows=10&keyword=a
-   @GetMapping("sub7")
+    // /main30/sub7?page=2&rows=10&keyword=a
+    @GetMapping("sub7")
     public void method7(MyDto29 dto1, MyDto30 dto2) {
 //        dto1.setFrom((dto1.getPage() - 1) * dto1.getRows());
 //        dto2.setKeyword("%" + dto2.getKeyword() + "%");
 
         List<String> names = dao.select7(dto1, dto2);
-       System.out.println("dto1.getPage() = " + dto1.getPage());
+        System.out.println("dto1.getPage() = " + dto1.getPage());
         names.forEach(System.out::println);
-   }
+    }
 
-   // /main30/sub8?name=son&country=korea
-   @GetMapping("sub8")
+    // /main30/sub8?name=son&country=korea
+    @GetMapping("sub8")
     public void method8(Mydto31 dto) {
 
-       int row = dao.insert1(dto);
-       System.out.println(row + "행이 입력되었습니다.");
-   }
+        int row = dao.insert1(dto);
+        System.out.println(row + "행이 입력되었습니다.");
+    }
 
-   // /main30/sub9
-   @GetMapping("sub9")
+    // /main30/sub9
+    @GetMapping("sub9")
     public void method9() {
 
-   }
+    }
 
-   @PostMapping("sub10")
+    @PostMapping("sub10")
     public void method10(MyDto32 emp) {
 
-        int row= dao.insert2(emp);
-       System.out.println(row + "행이 입력됨");
-   }
+        int row = dao.insert2(emp);
+        System.out.println(row + "행이 입력됨");
+    }
 
-   // /main30/sub11?id=5
-   @GetMapping("sub11")
+    // /main30/sub11?id=5
+    @GetMapping("sub11")
     public void method11(Integer id) { //파라미터 여기에 써줌
-       int rows = dao.delete1(id);
-       System.out.println( rows + "개 행이 지워짐");
-   }
+        int rows = dao.delete1(id);
+        System.out.println(rows + "개 행이 지워짐");
+    }
 
-   // /main30/sub12?pid=3
+    // /main30/sub12?pid=3
     //3번 상품이 삭제되는 메소드 완성
-   @GetMapping("sub12")
+    @GetMapping("sub12")
     public void method12(Integer pid) {
-       int rows = dao.delete2(pid);
-       System.out.println("rows = " + rows +"개 행 지워짐");
-   }
+        int rows = dao.delete2(pid);
+        System.out.println("rows = " + rows + "개 행 지워짐");
+    }
 
 
-   // /mina30/sub13?id=2
+    // /mina30/sub13?id=2
     @GetMapping("sub13")
-    public void method13(Integer id, Model model){
+    public void method13(Integer id, Model model) {
         // 직원 조회
         MyDto33Employee employee = dao.select8(id);
 
         model.addAttribute("employee", employee);
 
     }
+   /*
+   public String method13() {
+        }
+
+        return "/main30/sub14" 라고 하면 sub14.jsp로 가게 되는데
+        지금 public void method13() 으로 되어있고 리턴하는 것이 없음.
+        그럴땐 @GetMapping("sub13")에 쓴 sub13.jsp파일을  무조건 보여줌
+   */
+
 
     @PostMapping("sub14")
     public String method14(MyDto33Employee employee, RedirectAttributes rttr) { //수정한것을 자바빈에서 받음
--- src/main/webapp/WEB-INF/jsp/main30/sub13.jsp
+++ src/main/webapp/WEB-INF/jsp/main30/sub13.jsp
@@ -14,7 +14,9 @@
 </head>
 <body>
 <h3>직원 조회</h3>
-<form action="/main30/sub13">
+<form action="/main30/sub13"> <%-- form에서 이벤트가 발생해야 액션을 함. 버튼이 submit제출하는 속성이 있음. 버튼이 name=을 url에 제출함 . getMethod를 가지고--%>
+    <%-- 버튼을 누르는 순간 내가 쓴 id가 sub13으로 들어감. 13으로 들어오는 것을 받아줄수있는 메소드가 있는지 봐야함 . name과 파라미터명이 같아야함.
+     --%>
     <input type="text" name="id" placeholder="직원번호">
     <button>조회</button>
 </form>
@@ -22,11 +24,12 @@
 
 
 <h3>직원 수정</h3>
-<form action="/main30/sub14" method="post">
+<form action="/main30/sub14" method="post"> <%-- 본인----> 서버에 main30/sub14의 내용을 담은 http메서드를 서버에 던지는것 --%>
+    <%-- 서버는 응답하는 객체를 우리에게 다시 보내줌 --%>
 <%--    div*5>input[name]--%>
 
     <div>
-        <input type="text" name="customerId"  value="${employee.customerId}" readonly> <%--id는 변경되면 안 됨--%>
+        <input type="text" name="id"  value="${employee.id}" readonly> <%--id는 변경되면 안 됨--%>
     </div>
     <div>
         <input type="text" name="lastName" value="${employee.lastName}">
@@ -44,7 +47,9 @@
         <textarea name="notes" id="" cols="30" rows="10">${employee.notes}</textarea>
     </div>
     <div>
-        <button>수정</button>
+        <button>수정</button> <%--버튼누르는 순간 submit, 수정된 employee 객체가 담김. 그래서 처리할수 있는 메서드가 컨트롤러30 sub14에 있으면
+        메소드는 employee객체를 받아서 update()메소드에 employee를 넣은 후 실행함. 그리고 redirect를 줘서
+        그 페이지에 보일 수 있도록 함 --%>
     </div>
 
     <div>
